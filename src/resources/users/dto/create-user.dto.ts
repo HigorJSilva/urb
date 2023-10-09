afb93248785894400933a31aaca307963965dba8
Higor Silva <higor.junio@hotmail.com>
2023-10-09 10:15:38 -0300

    :sparkles: add create user validation

--- src/resources/users/dto/create-user.dto.ts
+++ src/resources/users/dto/create-user.dto.ts
@@ -1,1 +1,27 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @ApiProperty({
+    description: 'User`s email',
+  })
+  @IsEmail(undefined, {
+    message: 'Must be a valid email address',
+  })
+  email: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'User`s password',
+  })
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  password: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  username: string;
+}
