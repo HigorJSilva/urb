a23a83d828f870ac9d561a6156682662194c7ca1
Higor Silva <higor.junio@hotmail.com>
2023-10-09 09:25:45 -0300

    :sparkles: database connection with config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { getDbConfig } from './shared/database/connection';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync(getDbConfig() as TypeOrmModuleAsyncOptions),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
