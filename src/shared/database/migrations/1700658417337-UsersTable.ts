1366746da5083ddd8f206d8f51a6f1f2abe657c0
Higor Silva <higor.junio@hotmail.com>
2023-11-22 11:56:52 -0300

    :sparkles: add user table and entity

---
+++ src/shared/database/migrations/1700658417337-UsersTable.ts
@@ -0,0 +1,41 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class UsersTable1700658417337 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+            isNullable: false,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'username',
+            type: 'varchar',
+            isNullable: false,
+          },
+        ],
+      }),
+      false,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`DROP TABLE users`);
+  }
+}
