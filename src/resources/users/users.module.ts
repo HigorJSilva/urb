5f09776715f1d01ed797b33fe54eb0a3dffd9987
Higor Silva <higor.junio@hotmail.com>
2023-11-22 13:19:03 -0300

    :sparkles: add user service

--- src/resources/users/users.module.ts
+++ src/resources/users/users.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import Bcrypt from 'src/shared/providers/hash/bcrypt';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, Bcrypt],
 })
 export class UsersModule {}
--- src/resources/users/users.service.ts
+++ src/resources/users/users.service.ts
@@ -1,11 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { ValidateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import Bcrypt from 'src/shared/providers/hash/bcrypt';
+import { IHashProvider } from 'src/shared/interfaces/hash.provider';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+
+    @Inject(Bcrypt)
+    private readonly hash: IHashProvider,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const hashedPassword = await this.hash.generateHash(createUserDto.password);
+
+    return this.userRepository.save({
+      ...createUserDto,
+      password: hashedPassword,
+    });
   }
 
   validate(validateUserDto: ValidateUserDto) {
