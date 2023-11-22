cb94df28afabe74ca78aa6d751bd4b543e93266d
Higor Silva <higor.junio@hotmail.com>
2023-11-22 11:37:24 -0300

    :sparkles: fix db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { getDbConfig } from './shared/database/connection';
 import { ResourcesModule } from './resources/resources.module';
 import { UsersModule } from './resources/users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true,
+      envFilePath: ['.env'],
     }),
-    TypeOrmModule.forRootAsync(getDbConfig() as TypeOrmModuleAsyncOptions),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      entities: [`${__dirname}/resources/**/entities/*.entity{ .ts,.js}`],
+      migrations: [`${__dirname}/shared/database/migrations/*{ .ts,.js}`],
+      migrationsRun: true,
+    }),
     ResourcesModule,
     UsersModule,
   ],
