e8bb9585f848473bc1da8e4fe3d5c3807535d125
Higor Silva <higor.junio@hotmail.com>
2023-11-27 18:54:55 -0300

    :sparkles: add swagger to create user route

--- src/resources/users/dto/create-user.dto.ts
+++ src/resources/users/dto/create-user.dto.ts
@@ -3,25 +3,26 @@ import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
 import { isUnique } from 'src/shared/decorator/unique';
 
 export class CreateUserDto {
-  @IsString()
   @ApiProperty({
-    description: 'User`s email',
+    description: "User's email",
   })
+  @IsString()
   @IsEmail(undefined, {
     message: 'Must be a valid email address',
   })
   @isUnique({ tableName: 'users', column: 'email' })
   email: string;
-
   @IsString()
-  @ApiProperty({
-    description: 'User`s password',
-  })
   @IsString()
   @MinLength(4)
   @MaxLength(20)
   password: string;
 
+  @ApiProperty({
+    description: "User's username",
+    maximum: 20,
+    minimum: 8,
+  })
   @IsString()
   @MinLength(8)
   @MaxLength(20)
