a7d694b2a4cf7d0d6435f782a197bbf9d2dfddb9
Higor Silva <higor.junio@hotmail.com>
2023-12-04 14:23:48 -0300

    :sparkles: add jwt register globally

--- src/resources/auth/auth.module.ts
+++ src/resources/auth/auth.module.ts
@@ -3,11 +3,19 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import Bcrypt from 'src/shared/providers/hash/bcrypt';
 
-import { JwtService } from '@nestjs/jwt';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { UsersModule } from '../users/users.module';
+import { jwtConstants } from 'src/shared/config/auth/constants';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: jwtConstants.expiresIn },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, Bcrypt, JwtService],
 })
