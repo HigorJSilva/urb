a0198696f01464daf339c81d0050808d8ab8dba3
Higor Silva <higor.junio@hotmail.com>
2023-12-05 16:11:38 -0300

    :hammer: fix address module

--- src/resources/addresses/addresses.module.ts
+++ src/resources/addresses/addresses.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AddressesService } from './addresses.service';
-import { AddressesController } from './addresses.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Addresses } from './entities/address.entity';
 import { Cities } from '../cities/entities/cities';
@@ -8,7 +6,5 @@ import { States } from '../states/entities/states';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Addresses, Cities, States])],
-  controllers: [AddressesController],
-  providers: [AddressesService],
 })
 export class AddressesModule {}
