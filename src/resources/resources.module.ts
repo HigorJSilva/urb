887e82a74e397e8fd0a605e2353a055430ff5288
Higor Silva <higor.junio@hotmail.com>
2024-01-05 11:07:52 -0300

    :sparkles: add tenants module

--- src/resources/resources.module.ts
+++ src/resources/resources.module.ts
@@ -3,8 +3,15 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { AddressesModule } from './addresses/addresses.module';
 import { PropertiesModule } from './properties/properties.module';
+import { TenantModule } from './tenant/tenant.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, AddressesModule, PropertiesModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    AddressesModule,
+    PropertiesModule,
+    TenantModule,
+  ],
 })
 export class ResourcesModule {}
