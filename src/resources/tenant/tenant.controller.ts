cc68789e6f7a90eef4e5761de6482f14fce4ceb7
Higor Silva <higor.junio@hotmail.com>
2024-01-08 17:02:07 -0300

    :sparkles: add create tenant controller

--- src/resources/tenant/tenant.controller.ts
+++ src/resources/tenant/tenant.controller.ts
@@ -3,21 +3,52 @@ import {
   Get,
   Post,
   Body,
+  Headers,
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { TenantService } from './tenant.service';
 import { CreateTenantDto } from './dto/create-tenant.dto';
 import { UpdateTenantDto } from './dto/update-tenant.dto';
+import {
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { AuthGuard } from 'src/shared/config/auth/auth.guard';
+import { ReturnTenantDto } from './dto/return-tunant.dto';
 
 @Controller('tenant')
+@ApiTags('Properties')
+@ApiBearerAuth('JWT-auth')
+@UseGuards(AuthGuard)
 export class TenantController {
   constructor(private readonly tenantService: TenantService) {}
 
+  @ApiOkResponse({
+    type: ReturnTenantDto,
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Unauthorized Error',
+    schema: {
+      example: {
+        message: 'Unauthorized',
+        statuCode: 401,
+      },
+    },
+  })
   @Post()
-  create(@Body() createTenantDto: CreateTenantDto) {
-    return this.tenantService.create(createTenantDto);
+  create(
+    @Body() createTenantDto: CreateTenantDto,
+    @Headers('user') request: any,
+  ) {
+    return this.tenantService.create({
+      ...createTenantDto,
+      userId: request.sub.id,
+    });
   }
 
   @Get()
