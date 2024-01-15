c76a95d24aefe9665627f33bda706f49bba41816
Higor Silva <higor.junio@hotmail.com>
2024-01-15 17:08:50 -0300

    :sparkles: add rent entity

--- src/resources/rent/entities/rent.entity.ts
+++ src/resources/rent/entities/rent.entity.ts
@@ -1,1 +1,30 @@
-export class Rent {}
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Rent {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'user_id' })
+  userId: string;
+
+  @Column({ name: 'property_id' })
+  propertyId: string;
+
+  @Column({ name: 'tenant_id' })
+  tenantId: string;
+
+  @Column()
+  dueDate: Date;
+
+  @Column()
+  value: number;
+
+  @Column()
+  active = true;
+
+  @Column()
+  startDate: Date;
+
+  @Column()
+  endDate: Date;
+}
