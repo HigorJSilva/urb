0ef57a28fe248861a3437f2849ff42e5edbf30e7
Higor Silva <higor.junio@hotmail.com>
2024-01-16 11:14:26 -0300

    :rotating_light: add create rent tests

--- src/resources/rent/tests/rent.controller.spec.ts
+++ src/resources/rent/tests/rent.controller.spec.ts
@@ -1,20 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RentController } from '../rent.controller';
 import { RentService } from '../rent.service';
+import { createRent, returnedRent } from './mocks/rents.mock';
+import { JwtService } from '@nestjs/jwt';
 
 describe('RentController', () => {
   let controller: RentController;
+  let service: RentService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RentController],
-      providers: [RentService],
+      providers: [
+        JwtService,
+        {
+          provide: RentService,
+          useValue: {
+            create: jest.fn().mockResolvedValue(returnedRent),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<RentController>(RentController);
+    service = module.get<RentService>(RentService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+    expect(service).toBeDefined();
+  });
+
+  describe('create rent controller', () => {
+    it('should return the created rent if succeeds', async () => {
+      const response = await controller.create(createRent, {
+        sub: { id: '0c482f4b-786b-4562-b84f-0df9f699793b' },
+      });
+      expect(response).toEqual(returnedRent);
+    });
   });
 });
--- src/resources/rent/tests/rent.service.spec.ts
+++ src/resources/rent/tests/rent.service.spec.ts
@@ -1,18 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RentService } from '../rent.service';
+import { Rent } from '../entities/rent.entity';
+import { createRent, returnedRent } from './mocks/rents.mock';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 describe('RentService', () => {
   let service: RentService;
+  let rentRepository: Repository<Rent>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RentService],
+      providers: [
+        RentService,
+        {
+          provide: getRepositoryToken(Rent),
+          useValue: {
+            save: jest.fn().mockResolvedValue(returnedRent),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<RentService>(RentService);
+    rentRepository = module.get<Repository<Rent>>(getRepositoryToken(Rent));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(rentRepository).toBeDefined();
+  });
+
+  describe('create rent service', () => {
+    it('should return the created rent if succeeds', async () => {
+      const response = await service.create(createRent);
+      expect(response).toEqual(returnedRent);
+    });
   });
 });
